3456c46d86682be39f2be08b089a1c8489c21865
codymcelroy <cody.l.mcelroy@gmail.com>
2022-07-23 11:46:21 -0500

    main

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-ticker = 'WELL' 
-# data = pd.DataFrame()
-# data[ticker] = wb.DataReader(ticker, data_source='yahoo', start='2017-1-1')['Adj Close']
-
-# log_returns = np.log(1 + data.pct_change())
